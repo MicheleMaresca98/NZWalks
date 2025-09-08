be70b055e6b2520b6d18e10edb745cbfb66b7d8d
m.maresca <m.maresca@namirial.com>
2025-09-08 17:02:25 +0200

    feat: add role-based authorization

--- NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -10,7 +10,6 @@ namespace NZWalks.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-[Authorize]
 public class RegionsController : ControllerBase
 {
     private readonly IRegionRepository regionRepository;
@@ -23,6 +22,7 @@ public class RegionsController : ControllerBase
     }
 
     [HttpGet]
+    [Authorize(Roles = "Reader")]
     public async Task<IActionResult> GetAll()
     {
         var regionsDomain = await regionRepository.GetAllAsync();
@@ -31,6 +31,7 @@ public class RegionsController : ControllerBase
 
     [HttpPost]
     [ValidateModel]
+    [Authorize(Roles = "Writer")]
     public async Task<IActionResult> Create([FromBody] AddRegionRequestDto addRegionRequestDto)
     {
         var regionDomainModel = mapper.Map<Region>(addRegionRequestDto);
@@ -40,6 +41,7 @@ public class RegionsController : ControllerBase
 
     [HttpGet]
     [Route("{id:Guid}")]
+    [Authorize(Roles = "Reader")]
     public async Task<IActionResult> GetById([FromRoute] Guid id)
     {
         var region = await regionRepository.GetByIdAsync(id);
@@ -53,6 +55,7 @@ public class RegionsController : ControllerBase
     [HttpPut]
     [Route("{id:Guid}")]
     [ValidateModel]
+    [Authorize(Roles = "Writer")]
     public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
     {
         var regionDomainModel = mapper.Map<Region>(updateRegionRequestDto);
@@ -66,6 +69,7 @@ public class RegionsController : ControllerBase
 
     [HttpDelete]
     [Route("{id:Guid}")]
+    [Authorize(Roles = "Writer")]
     public async Task<IActionResult> Delete([FromRoute] Guid id)
     {
         var regionDomainModel = await regionRepository.DeleteAsync(id);
