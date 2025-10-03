0711b82d47e6a04683f586a2944e7f8ad622b58a
m.maresca <m.maresca@namirial.com>
2025-10-03 14:52:26 +0200

    feat(Web-API-Versioning): add countries API v2

--- Web-API-Versioning.API/Models/DTOs/CountryDto.cs
+++ Web-API-Versioning.API/Models/DTOs/CountryDto.cs
@@ -1,8 +1,14 @@
 namespace Web_API_Versioning.API.Models.DTOs
 {
-    public class CountryDto
+    public class CountryDtoV1
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+    }
+
+    public class CountryDtoV2
+    {
+        public int Id { get; set; }
+        public string CountryName { get; set; } = string.Empty;
     }
 }
--- Web-API-Versioning.API/V1/Controllers/CountriesController.cs
+++ Web-API-Versioning.API/V1/Controllers/CountriesController.cs
@@ -4,21 +4,21 @@ using Web_API_Versioning.API;
 using Web_API_Versioning.API.Models.Domain;
 using Web_API_Versioning.API.Models.DTOs;
 
-namespace Web_API_Versioning.API.Controllers
+namespace Web_API_Versioning.API.V1.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     public class CountriesController : ControllerBase
     {
         [HttpGet]
         public IActionResult GetCountries()
         {
             var countriesDomainModel = CountriesData.Get();
-            var response = new List<CountryDto>();
+            var response = new List<CountryDtoV1>();
 
             foreach (var countryDomain in countriesDomainModel)
             {
-                response.Add(new CountryDto
+                response.Add(new CountryDtoV1
                 {
                     Id = countryDomain.Id,
                     Name = countryDomain.Name
